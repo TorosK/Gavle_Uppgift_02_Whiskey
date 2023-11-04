891c6e6b94e699b90ce72eee3dd7fbbfce6a9073
Toros Kutlu <toros_kutlu@hotmail.com>
2023-11-04 14:44:12 +0100

    lists for dropdowns

--- app/src/main/java/com/example/gavle_uppgift_02_whiskey/MainActivity.kt
+++ app/src/main/java/com/example/gavle_uppgift_02_whiskey/MainActivity.kt
@@ -31,6 +31,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var artistSpinner: Spinner
     private lateinit var sizeSpinner: Spinner
 
+    private val pricesPerCl by lazy { resources.getIntArray(R.array.prices_per_cl) }
+    private val priceTextFormat by lazy { resources.getString(R.string.price_text_format) }
+    private val sizeTextFormat by lazy { resources.getString(R.string.size_text_format) }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -100,21 +105,32 @@ class MainActivity : AppCompatActivity() {
         artistImage.setImageResource(artistResId)
 
         // Uppdaterar priset för den valda whiskyn
-        val whiskyIndex = whiskies.indexOf(selectedWhisky)
-        if (whiskyIndex != -1) {
-            val whiskyPrice = prices[whiskyIndex]
-            priceText.text = getString(R.string.price_text_format, whiskyPrice)
+        // Find index of selected whisky
+        val whiskyIndex = whiskies.indexOf(selectedWhisky).takeIf { it >= 0 } // Include the first item
+        val sizeIndex = sizes.indexOf(selectedSize).takeIf { it >= 0 } // Include the first item
+
+        // Update price text only if a whisky and a size are selected and not placeholders
+        if (whiskyIndex != null && sizeIndex != null && selectedWhisky.isNotEmpty() && selectedSize.isNotEmpty()) {
+            val pricePerCl = pricesPerCl[whiskyIndex]
+            val sizeInCl = sizes[sizeIndex].filter { it.isDigit() }.toIntOrNull() ?: 0 // Safely convert to Int
+            val totalPrice = pricePerCl * sizeInCl
+            priceText.text = priceTextFormat.format(totalPrice.toFloat())
+
+            // Update size and total price text
+            infoText.text = sizeTextFormat.format(selectedSize, totalPrice.toFloat())
         }
     }
 
     private fun getWhiskyImageResource(whiskyName: String): Int {
         // Antag att du har en funktion som mappar whiskyName till en drawable-resurs.
         // Implementera den här logiken baserat på ditt specifika namngivningsschema.
-        return resources.getIdentifier(whiskyName.toLowerCase().replace(" ", "_"), "drawable", packageName)
+        return resources.getIdentifier(whiskyName.lowercase().replace(" ", "_"), "drawable",
+        packageName)
     }
 
     private fun getArtistImageResource(artistName: String): Int {
         // Samma som ovan, mappa artistName till en drawable-resurs.
-        return resources.getIdentifier(artistName.toLowerCase().replace(" ", "_"), "drawable", packageName)
+        return resources.getIdentifier(artistName.lowercase().replace(" ", "_"), "drawable",
+            packageName)
     }
 }
