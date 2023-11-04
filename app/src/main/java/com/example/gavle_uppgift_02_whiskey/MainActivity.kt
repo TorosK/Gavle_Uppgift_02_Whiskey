13cd8571f4c27be6126e6bd806753d63a4559d6b
Toros Kutlu <toros_kutlu@hotmail.com>
2023-11-04 13:44:50 +0100

    major improvements

--- app/src/main/java/com/example/gavle_uppgift_02_whiskey/MainActivity.kt
+++ app/src/main/java/com/example/gavle_uppgift_02_whiskey/MainActivity.kt
@@ -1,42 +1,120 @@
+// MainActivity.kt
+
 package com.example.gavle_uppgift_02_whiskey
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    var selectedWhisky = ""
+
+    private lateinit var whiskyImage: ImageView
+    private lateinit var artistImage: ImageView
+    private lateinit var priceText: TextView
+    private lateinit var infoText: TextView
+
+    private val whiskies by lazy { resources.getStringArray(R.array.whiskies) }
+    private val prices by lazy { resources.getStringArray(R.array.prices) }
+    private val artists by lazy { resources.getStringArray(R.array.artists) }
+    private val sizes by lazy { resources.getStringArray(R.array.sizes) }
+
+    private var selectedWhisky = ""
+    private var selectedArtist = ""
+    private var selectedSize = ""
+
+    private lateinit var whiskySpinner: Spinner
+    private lateinit var artistSpinner: Spinner
+    private lateinit var sizeSpinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val whiskies = resources.getStringArray(R.array.whiskies)
-        var infoText = findViewById<TextView>(R.id.textView);
-        val spinner: Spinner = findViewById(R.id.spinner)
-        var adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, whiskies)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = adapter;
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+        // Initialisera ImageViews och TextViews
+        whiskyImage = findViewById(R.id.whiskyImage)
+        artistImage = findViewById(R.id.artistImage)
+        priceText = findViewById(R.id.priceText)
+        infoText = findViewById(R.id.infoText)
+
+        // Initialisera Spinners
+        whiskySpinner = findViewById(R.id.whiskySpinner)
+        artistSpinner = findViewById(R.id.artistSpinner)
+        sizeSpinner = findViewById(R.id.sizeSpinner)
+
+        val whiskyAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, whiskies)
+        whiskyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        whiskySpinner.adapter = whiskyAdapter
+
+        val artistAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, artists)
+        artistAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        artistSpinner.adapter = artistAdapter
+
+        val sizeAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, sizes)
+        sizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        sizeSpinner.adapter = sizeAdapter
+
+        whiskySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                selectedWhisky = whiskies[position]
+                updateInfo()
             }
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int,
-                                        id: Long) {
-                var selectedIndex = spinner.getSelectedItemPosition();
-                if (selectedIndex > 0)
-                    selectedWhisky = whiskies[selectedIndex]
-                else
-                    selectedWhisky = ""
-                printInfo(infoText)
+        }
+
+        artistSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                selectedArtist = artists[position]
+                updateInfo()
+            }
+        }
+
+        sizeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                selectedSize = sizes[position]
+                updateInfo()
             }
         }
     }
-    fun printInfo(tView: TextView) {
-        if (!selectedWhisky.equals(""))
-            tView.text = "Your favourite whisky is " + selectedWhisky
-        else
-            tView.text = "Please choose your favourite whisky."
+
+    // UpdateInfo-funktionen definieras här för att tillåta åtkomst till ImageView och TextView.
+    private fun updateInfo() {
+        // Uppdaterar infoText med vald whisky, konstnär och storlek
+        infoText.text = getString(R.string.info_text_format, selectedWhisky, selectedArtist, selectedSize)
+
+        // Uppdaterar bilden för den valda whiskyn
+        val whiskyResId = getWhiskyImageResource(selectedWhisky)
+        whiskyImage.setImageResource(whiskyResId)
+
+        // Uppdaterar bilden för den valda konstnären
+        val artistResId = getArtistImageResource(selectedArtist)
+        artistImage.setImageResource(artistResId)
+
+        // Uppdaterar priset för den valda whiskyn
+        val whiskyIndex = whiskies.indexOf(selectedWhisky)
+        if (whiskyIndex != -1) {
+            val whiskyPrice = prices[whiskyIndex]
+            priceText.text = getString(R.string.price_text_format, whiskyPrice)
+        }
+    }
+
+    private fun getWhiskyImageResource(whiskyName: String): Int {
+        // Antag att du har en funktion som mappar whiskyName till en drawable-resurs.
+        // Implementera den här logiken baserat på ditt specifika namngivningsschema.
+        return resources.getIdentifier(whiskyName.toLowerCase().replace(" ", "_"), "drawable", packageName)
+    }
+
+    private fun getArtistImageResource(artistName: String): Int {
+        // Samma som ovan, mappa artistName till en drawable-resurs.
+        return resources.getIdentifier(artistName.toLowerCase().replace(" ", "_"), "drawable", packageName)
     }
 }
