99a64415864e1f263d09b8df4c650724245c0fb3
Toros Kutlu <toros_kutlu@hotmail.com>
2023-11-04 13:04:56 +0100

    // Updated SDK from 33 to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.gavle_uppgift_02_whiskey"
-    compileSdk = 33
+    compileSdk = 34 // Updated from 33 to 34
 
     defaultConfig {
         applicationId = "com.example.gavle_uppgift_02_whiskey"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34 // Updated from 33 to 34
         versionCode = 1
         versionName = "1.0"
 
@@ -37,7 +37,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
